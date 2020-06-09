5b0f4f4b4efc8f1e496cf809816740b0efde146e
felcg <xfelipecg@gmail.com>
2020-06-09 20:09:00 -0300

    Cria o componente GameCard

---
+++ src/components/GameCard/GameCard.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import './GameCard.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const GameCard = ({ game }) => (
+  <Card className="GameCard" key={game.id}>
+    <div className="GameNameContainer">
+      <h5 className="GameName">{game.name} </h5>
+      <FontAwesomeIcon className="LinkIcon" icon={['fas', 'external-link-alt']} />
+    </div>
+    <div className="CoverAndInfo">
+      {game.cover
+        ? <img src={game.cover.url} alt={`cover of the game ${game.name}`} />
+        : <div>No Image Yet</div>}
+      <div className="GameInfo">
+        <div className="GameSummary">{game.summary}</div>
+      </div>
+
+    </div>
+    <div className="ReleaseDateAndRating">
+      <div className="ReleaseDate">{game.release_dates[0].human}</div>
+
+      <div className="GameRating">
+        <span>{game.total_rating.toFixed()}</span>
+      </div>
+    </div>
+
+  </Card>
+)
+
+export default GameCard
