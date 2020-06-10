ead4676be107ea48adaa80cd17592c87c030c17d
felcg <xfelipecg@gmail.com>
2020-06-10 15:22:29 -0300

    Melhora a funcao getAllImages

--- src/components/index.js
+++ src/components/index.js
@@ -1,24 +1,28 @@
 /* eslint-disable react/destructuring-assignment */
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+
 import { Container } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+
+import Pagination from './Pagination/Pagination'
 import GameCard from './GameCard/GameCard'
 
-const baseUrl = 'http://localhost:3001/api/coop'
 
 const Component = (props) => {
   const [pager, setPager] = useState({})
   const [pageOfGames, setPageOfGames] = useState([])
+  const baseUrl = 'http://localhost:3001/api/games'
+
 
   const getAllImages = (arr) => {
-    arr.map((game) => {
-      if (game.cover) {
-        game.cover.url = game.cover.url.split('/')
-        game.cover.url[6] = 't_cover_big'
-        game.cover.url = game.cover.url.join('/')
-      }
+    const newArr = [...arr]
+    newArr.map((game) => {
+      game.cover.url = game.cover.url.split('/')
+      game.cover.url[6] = 't_cover_big'
+      game.cover.url = game.cover.url.join('/')
+      return null
     })
+    setPageOfGames(newArr)
   }
 
   const getAll = async () => {
@@ -34,39 +38,15 @@ const Component = (props) => {
     getAll()
   }, [props.location])
 
+
   return (
     <>
-      <div className="card-footer pb-0 pt-3">
-        {pager.pages && pager.pages.length && (
-          <ul className="pagination">
-            <li className={`page-item first-item ${pager.currentPage === 1 ? 'disabled' : ''}`}>
-              <Link to={{ search: '?page=1' }} className="page-link">First</Link>
-            </li>
-            <li className={`page-item previous-item ${pager.currentPage === 1 ? 'disabled' : ''}`}>
-              <Link to={{ search: `?page=${pager.currentPage - 1}` }} className="page-link">Previous</Link>
-            </li>
-            {pager.pages.map((page) => (
-              <li key={page} className={`page-item number-item ${pager.currentPage === page ? 'active' : ''}`}>
-                <Link to={{ search: `?page=${page}` }} className="page-link">{page}</Link>
-              </li>
-            ))}
-            <li className={`page-item next-item ${pager.currentPage === pager.totalPages ? 'disabled' : ''}`}>
-              <Link to={{ search: `?page=${pager.currentPage + 1}` }} className="page-link">Next</Link>
-            </li>
-            <li className={`page-item last-item ${pager.currentPage === pager.totalPages ? 'disabled' : ''}`}>
-              <Link to={{ search: `?page=${pager.totalPages}` }} className="page-link">Last</Link>
-            </li>
-          </ul>
-        )}
-      </div>
-
       <Container>
-        {console.log(pager)}
         {pageOfGames.map((game, index) => (
           <GameCard key={game.id} game={game} index={index} />
         ))}
       </Container>
-
+      {pager.pages && <Pagination pager={pager} />}
     </>
   )
 }
