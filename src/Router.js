9310afe5403595e555ceb45f3afa9623514df095
felcg <xfelipecg@gmail.com>
2020-06-10 20:41:00 -0300

    Adiciona route para search

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import Component from './components/index'
+import Index from './components/index'
+import Search from './components/Search/Search'
 
 export default () => (
   <Switch>
-    <Route exact path="/" component={Component} />
+    <Route exact path="/" component={Index} />
+    <Route exact path="/search" component={Search} />
   </Switch>
 
 )
