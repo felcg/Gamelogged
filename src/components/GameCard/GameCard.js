6fbbbee25c5548838f83c634d463160538919756
felcg <xfelipecg@gmail.com>
2020-06-11 16:30:34 -0300

    Modifica layout do componente GameCard

--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Button } from 'react-bootstrap'
 import './GameCard.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+// const secondsToHours = (seconds) => {
+//   const hours = Math.floor(seconds / 60 / 60)
+//   const minutes = Math.floor(seconds / 60) - (hours * 60)
+
+//   return `${hours}h${minutes}m`
+// }
 
 const GameCard = ({ game }) => (
   <Card className="GameCard" key={game.id}>
     <div className="GameNameContainer">
       <h5 className="GameName">{game.name} </h5>
-      <FontAwesomeIcon className="LinkIcon" icon={['fas', 'external-link-alt']} />
+      <div className="GameRating">
+        {game.total_rating && <span>{game.total_rating.toFixed()}</span>}
+      </div>
     </div>
     <div className="CoverAndInfo">
       {game.cover
@@ -18,12 +27,31 @@ const GameCard = ({ game }) => (
       </div>
 
     </div>
-    <div className="ReleaseDateAndRating">
+    <div className="ReleaseDateAndButton">
       {game.release_dates && <div className="ReleaseDate">{game.release_dates[0].human}</div>}
 
-      <div className="GameRating">
-        {game.total_rating && <span>{game.total_rating.toFixed()}</span>}
-      </div>
+      {/* {game.time_to_beat && (
+        <div className="TimeToBeat">
+          Time to beat:
+          {game.time_to_beat.completely !== 0
+            ? <div>Completely: {secondsToHours(game.time_to_beat.completely)} </div>
+            : <div />}
+
+          {game.time_to_beat.normally !== 0
+            ? <div>Normally: {secondsToHours(game.time_to_beat.normally)}</div>
+            : <div />}
+
+          {game.time_to_beat.hastly !== 0
+            ? <div>Hastly: {secondsToHours(game.time_to_beat.hastly)}</div>
+            : <div />}
+        </div>
+      )} */}
+
+
+      <Button>
+        <span>Info <FontAwesomeIcon className="LinkIcon" icon={['fas', 'external-link-alt']} /></span>
+      </Button>
+
     </div>
 
   </Card>
