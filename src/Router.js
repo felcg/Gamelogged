92c8b03846d5f58be4f2da117b6336575b53ab0e
felcg <xfelipecg@gmail.com>
2020-06-11 16:30:08 -0300

    Modifica componente Index

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import Index from './components/index'
+import Index from './components/Index/index'
 import Search from './components/Search/Search'
 
 export default () => (
