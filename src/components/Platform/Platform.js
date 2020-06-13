53d80e9647c814ef2b5485021453e477122d8660
felcg <xfelipecg@gmail.com>
2020-06-13 00:00:56 -0300

    Adiciona lista de plataformas para filtro

--- src/components/Platform/Platform.js
+++ src/components/Platform/Platform.js
@@ -14,8 +14,20 @@ import '../Index/Index.scss'
 const Index = (props) => {
   const [pager, setPager] = useState({})
   const [pageOfGames, setPageOfGames] = useState([])
+
   const baseUrl = 'http://localhost:3001/api/'
   const params = useParams()
+
+  const platforms = [{ slug: 'win', name: 'PC' }, { slug: 'linux', name: 'Linux' }, { slug: 'ps', name: 'Playstation' },
+    { slug: 'ps2', name: 'Playstation 2' }, { slug: 'ps3', name: 'Playstation 3' }, { slug: 'ps4--1', name: 'Playstation 4' },
+    { slug: 'playstation-5', name: 'Playstation 5' }, { slug: 'psp', name: 'PSP' }, { slug: 'psvita', name: 'PS Vita' },
+    { slug: 'xbox', name: 'Xbox' }, { slug: 'xbox360', name: 'Xbox 360' }, { slug: 'xboxone', name: 'Xbox One' }, { slug: 'xbox-series-x', name: 'Xbox Series X' },
+    { slug: 'nes', name: 'NES' }, { slug: 'snes--1', name: 'SNES' }, { slug: 'n64', name: 'Nintendo 64' }, { slug: 'ngc', name: 'GameCube' },
+    { slug: 'wii', name: 'Wii' }, { slug: 'wiiu', name: 'Wii U' }, { slug: 'switch', name: 'Switch' }, { slug: 'gb', name: 'Game Boy' },
+    { slug: 'gbc', name: 'Game Boy Color' }, { slug: 'gba', name: 'Game Boy Advance' }, { slug: 'nds', name: 'Nintendo DS' },
+    { slug: '3ds', name: 'Nintendo 3DS' }, { slug: 'smd', name: 'Mega Drive / Genesis' }, { slug: 'saturn', name: 'Sega Saturn' }, { slug: 'dc', name: 'Dreamcast' },
+    { slug: 'neo-geo-pocket', name: 'Neo Geo Pocket' }, { slug: 'neo-geo-pocket-color', name: 'Neo Geo Pocket Color' },
+    { slug: 'arcade', name: 'Arcade' }, { slug: 'ios', name: 'iOs' }, { slug: 'android', name: 'Android' }]
 
   const getAllImages = (arr) => {
     const newArr = [...arr]
@@ -29,6 +41,7 @@ const Index = (props) => {
     })
     setPageOfGames(newArr)
   }
+
 
   const getFilterPlatform = async () => {
     console.log('props', props)
@@ -51,10 +64,9 @@ const Index = (props) => {
     <>
       <NavBar />
       <Container className="contentWrapper">
-        <Link to={{ pathname: 'wii' }} className="page-link">Wii</Link>
-        <Link to={{ pathname: 'nds' }} className="page-link">Nds</Link>
-        <Link to={{ pathname: 'n64' }} className="page-link">n64</Link>
-        <Link to={{ pathname: 'ps2' }} className="page-link">ps2</Link>
+        {platforms.map((platform) => (
+          <Link key={platform.slug} to={{ pathname: `${platform.slug}` }} className="page-link">{platform.name}</Link>
+        ))}
         {pageOfGames.map((game, index) => (
           <GameCard key={game.id} game={game} index={index} />
         ))}
