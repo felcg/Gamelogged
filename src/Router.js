f6107e2a12744c60ed1acc2ffb8f264395054eb7
felcg <xfelipecg@gmail.com>
2020-06-19 05:54:29 -0300

    Adiciona rota para GameFullInfo

--- src/Router.js
+++ src/Router.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom'
 
 import Frontpage from './components/Frontpage/Frontpage'
 import Search from './components/Search/Search'
+import GameFullInfo from './components/GameFullInfo/GameFullInfo'
 
 export default () => (
   <Switch>
-    <Route exact path="/" component={Frontpage} />
     <Route exact path="/search" component={Search} />
+    <Route exact path="/games/:gameId" component={GameFullInfo} />
+    <Route exact path="/" component={Frontpage} />
   </Switch>
 
 )
