4771d429bff8665eed043f13f013017c3fb8d7af
felcg <xfelipecg@gmail.com>
2020-06-19 05:53:45 -0300

    Coloca navbar fixa em todas as paginas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Router from './Router'
+import NavBar from './components/NavBar/NavBar'
 import './assets/custom.scss'
 
 export default () => (
   <>
+    <NavBar />
     <Router />
   </>
 )
