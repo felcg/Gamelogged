d9063258a53ace9c50bf8e37a8a22c235d984963
felcg <xfelipecg@gmail.com>
2020-06-24 00:42:18 -0300

    Atualiza rotas

--- src/Router.js
+++ src/Router.js
@@ -4,13 +4,11 @@ import { Route, Switch, withRouter } from 'react-router-dom'
 import Frontpage from './components/Frontpage/Frontpage'
 import Search from './components/Search/Search'
 import GameFullInfo from './components/GameFullInfo/GameFullInfo'
-import Test from './components/Test/tests'
 
 export default () => (
   <Switch>
     <Route exact path="/games/:gameId" component={withRouter(GameFullInfo)} />
     <Route exact path="/search" component={Search} />
-    <Route exact path="/test" component={Test} />
     <Route exact path="/platforms/:platform" component={withRouter(Frontpage)} />
     <Route exact path="/" component={withRouter(Frontpage)} />
   </Switch>
