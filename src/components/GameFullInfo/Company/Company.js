f89675ce7114edcebec1d4810e19d15bbe2f00e4
felcg <xfelipecg@gmail.com>
2020-06-24 01:52:27 -0300

    Faz componente company usar gameService para buscar empresas na api

--- src/components/GameFullInfo/Company/Company.js
+++ src/components/GameFullInfo/Company/Company.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import gameService from '../../../services/games'
 
 const Company = ({ id }) => {
   const [company, setCompany] = useState([])
   const getCompany = async () => {
-    const baseUrl = 'http://localhost:3001/api/'
-    const request = await axios.get(`${baseUrl}companies/${id}`)
-    const returnedCompany = request.data
+    const returnedCompany = await gameService.getCompany(id)
     setCompany(returnedCompany[0])
   }
 
