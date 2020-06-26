132ca31ae9ba03dd1e05837a498ee2a89ec6fa54
felcg <xfelipecg@gmail.com>
2020-06-25 23:13:02 -0300

    Reorganiza rotas

--- src/Router.js
+++ src/Router.js
@@ -2,17 +2,15 @@ import React from 'react'
 import { Route, Switch, withRouter } from 'react-router-dom'
 
 import Frontpage from './components/Frontpage/Frontpage'
-import Search from './components/Search/Search'
 import GameFullInfo from './components/GameFullInfo/GameFullInfo'
 import Home from './components/Home/Home'
 
 export default () => (
   <Switch>
     <Route exact path="/games/:gameId" component={withRouter(GameFullInfo)} />
-    <Route exact path="/search" component={Search} />
     <Route exact path="/platforms/:platform" component={withRouter(Frontpage)} />
-    <Route exact path="/Home" component={withRouter(Home)} />
-    <Route exact path="/" component={withRouter(Frontpage)} />
+    <Route exact path="/platforms" component={withRouter(Frontpage)} />
+    <Route exact path="/" component={withRouter(Home)} />
   </Switch>
 
 )
