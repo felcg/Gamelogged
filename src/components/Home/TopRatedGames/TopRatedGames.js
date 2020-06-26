b33db32bd6d8f026eea2b4f7b6aa486cc23497c3
felcg <xfelipecg@gmail.com>
2020-06-25 23:10:17 -0300

    Cria componentes RecentGames, TopRatedGames e VideoContainer para pagina Home

---
+++ src/components/Home/TopRatedGames/TopRatedGames.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+import gameService from '../../../services/games'
+import GameCard from '../../GameCard/GameCard'
+import './TopRatedGames.scss'
+
+const TopRatedGames = () => {
+  const [games, setGames] = useState([])
+
+  const getGames = async () => {
+    const response = await gameService.getTopTen()
+    setGames(response)
+  }
+
+  useEffect(() => {
+    getGames()
+  }, [])
+
+  return (
+    <div className="TopRatedGamesContainer">
+      <h1>Top10 Games with a lot of votes</h1>
+      <div className="GameCardContainer container">
+        {games && games.map((game, index) => (
+          <GameCard key={game.id} game={game} index={index} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default TopRatedGames
