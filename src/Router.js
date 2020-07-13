eba192fb1fea47ba2dbcda8fc760aa776aead081
felcg <xfelipecg@gmail.com>
2020-07-13 19:16:22 -0300

    Adiciona rotas para verify e login

--- src/Router.js
+++ src/Router.js
@@ -5,9 +5,13 @@ import Frontpage from './components/Frontpage/Frontpage'
 import GameFullInfo from './components/GameFullInfo/GameFullInfo'
 import Home from './components/Home/Home'
 import SignUp from './components/SignUp/SignUp'
+import Verify from './components/Verify/Verify'
+import Login from './components/Login/Login'
 
 export default () => (
   <Switch>
+    <Route exact path="/login" component={withRouter(Login)} />
+    <Route exact path="/verify" component={withRouter(Verify)} />
     <Route exact path="/signup" component={withRouter(SignUp)} />
     <Route exact path="/games/:gameId" component={withRouter(GameFullInfo)} />
     <Route exact path="/platforms/:platform" component={withRouter(Frontpage)} />
