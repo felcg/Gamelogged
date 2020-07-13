024481eba8e2a55a1ca4cbdba01ecbfea98694d9
felcg <xfelipecg@gmail.com>
2020-07-13 19:17:17 -0300

    Adiciona componente Login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Button } from 'react-bootstrap'
+import {
+  Formik, Field, Form, ErrorMessage,
+} from 'formik'
+import * as Yup from 'yup'
+import loginService from '../../services/login'
+import { login } from '../../reducers/userReducer'
+import './Login.scss'
+
+const loginSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Required'),
+  password: Yup.string()
+    .required('Required')
+    .min(6, 'Password needs to be at least 6 characters long'),
+})
+
+const Login = () => {
+  const [error, setError] = useState(null)
+  const [success, setSuccess] = useState(null)
+  const dispatch = useDispatch()
+
+  return (
+    <div className="form">
+
+      {success === null
+        ? (
+          <div className="form__signup">
+            <h1 className="form__title">Join Gamelogged.com!</h1>
+            <Formik initialValues={{
+              name: '', username: '', email: '', password: '',
+            }}
+
+              validationSchema={loginSchema}
+
+              onSubmit={async (values, { setSubmitting }) => {
+                const input = {
+                  email: values.email,
+                  password: values.password,
+                }
+
+                try {
+                  const user = await loginService.login(input)
+                  dispatch(login(user))
+                  setSubmitting(false)
+                  setSuccess('Successfully logged. See your account here.')
+                  window.localStorage.setItem(
+                    'loggedUser', JSON.stringify(user),
+                  )
+                } catch (error) {
+                  setError(error.response.data)
+                }
+              }}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <label>Email</label>
+                  <Field type="email" name="email" />
+                  <ErrorMessage name="email" component="div" className="form__error" />
+
+
+                  <label>Password</label>
+                  <Field type="password" name="password" />
+                  <ErrorMessage name="password" component="div" className="form__error" />
+
+                  <Button variant="primary" type="submit" disabled={isSubmitting}>
+                    Login
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </div>
+        )
+        : (
+          <div className="form__result--success">
+            <p>{success}</p>
+
+          </div>
+        ) }
+
+      {error && (
+      <div className="form__error--db">
+        <p>{error}</p>
+      </div>
+      )}
+
+    </div>
+  )
+}
+
+export default Login
