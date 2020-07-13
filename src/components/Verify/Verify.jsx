cb90461626ce25458a55f71dd11e49cf761979f8
felcg <xfelipecg@gmail.com>
2020-07-13 19:17:48 -0300

    Adiciona componente Verify

---
+++ src/components/Verify/Verify.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/jsx-indent-props */
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import { Link, useLocation } from 'react-router-dom'
+
+import './Verify.scss'
+
+const Verify = () => {
+  const location = useLocation()
+  const params = new URLSearchParams(location.search)
+  const email = params.get('email')
+
+  return (
+
+    <div className="verify__success">
+      <p>The account linked to email {email} has been verified.</p>
+
+      <Link to="/"><Button>BACK TO HOME</Button></Link>
+    </div>
+
+  )
+}
+
+export default Verify
