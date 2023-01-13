cb3f9c4b8f27e462fe63ef810d747a28dae25b53
felcg <xfelipecg@gmail.com>
2023-01-12 22:38:21 -0300

    Updates

---
+++ precache-manifest.055cfaea29fe63107fb3a69766606023.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711ffeb1c5901a58e99d58e0423a4012",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2ad036385475445526f",
+    "url": "/static/css/main.cf272ada.chunk.css"
+  },
+  {
+    "revision": "4666d121f8b7f42b9333",
+    "url": "/static/js/2.2d4d7864.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.2d4d7864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ad036385475445526f",
+    "url": "/static/js/main.b5b884b5.chunk.js"
+  },
+  {
+    "revision": "2738e2510f3018dc68d7",
+    "url": "/static/js/runtime-main.de61d3ad.js"
+  },
+  {
+    "revision": "c951b2133d5f03942ec7ce09f2020a71",
+    "url": "/static/media/suikoden-2-box-art-crop_1280.0.0.c951b213.jpg"
+  }
+]);
