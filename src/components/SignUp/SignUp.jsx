067c37edd7570647c41889ac5b299c8c5c6addb5
felcg <xfelipecg@gmail.com>
2023-01-20 19:57:08 -0300

    finish signup

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/jsx-indent-props */
 import React, { useState } from 'react'
 import { Button } from 'react-bootstrap'
-import { Link, useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Formik, Field, Form, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
+import Loading from '../Loader/Loading'
 import signupService from '../../services/signup'
 import './SignUp.scss'
 
@@ -23,24 +24,21 @@ const SignupSchema = Yup.object().shape({
 })
 
 const SignUp = () => {
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(false)
   const [success, setSuccess] = useState(null)
-  const location = useLocation()
-  console.log('location', location)
-
-  const params = new URLSearchParams(location.search)
-  const foo = params.get('email') // bar
-  console.log(foo)
+  const [isLoading, setIsLoading] = useState(false)
 
   return (
+
     <div className="form">
       {success === null ? (
         <div className="form__signup">
           <h1 className="form__title">Join Gamelogged.com!</h1>
 
-          {error && <div className="form__result">{error}</div>}
+          {error && <div className="form__result">There has been an error, please try again later.</div>}
 
-          <Formik
+          <Loading isLoading={isLoading}>
+            <Formik
             initialValues={{
               name: '',
               username: '',
@@ -49,6 +47,7 @@ const SignUp = () => {
             }}
             validationSchema={SignupSchema}
             onSubmit={async (values, { setSubmitting }) => {
+              setIsLoading(true)
               const user = {
                 name: values.name,
                 email: values.email,
@@ -60,54 +59,57 @@ const SignUp = () => {
                 await signupService.signup(user)
                 setSubmitting(false)
                 setSuccess(
-                  "Thanks for joining! Check your email to verify your account. If you can't find the email, please check your spam folder."
+                  'Thanks for joining! Check your email to verify your account. If you can\'t find the email, please check your spam folder.'
                 )
               } catch (error) {
-                console.log(error.response.data)
-                setError(error.response.data)
+                console.log('error', error)
+                setError(true)
+              } finally {
+                setIsLoading(false)
               }
             }}
-          >
-            {({ isSubmitting }) => (
-              <Form>
-                <label>Name</label>
-                <Field type="text" name="name" id="name" />
-                <ErrorMessage
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <label>Name</label>
+                  <Field type="text" name="name" id="name" />
+                  <ErrorMessage
                   name="name"
                   component="div"
                   className="form__error"
-                />
+                  />
 
-                <label>Username</label>
-                <Field type="text" name="username" />
-                <ErrorMessage
+                  <label>Username</label>
+                  <Field type="text" name="username" />
+                  <ErrorMessage
                   name="username"
                   component="div"
                   className="form__error"
-                />
+                  />
 
-                <label>Email</label>
-                <Field type="email" name="email" />
-                <ErrorMessage
+                  <label>Email</label>
+                  <Field type="email" name="email" />
+                  <ErrorMessage
                   name="email"
                   component="div"
                   className="form__error"
-                />
+                  />
 
-                <label>Password</label>
-                <Field type="password" name="password" />
-                <ErrorMessage
+                  <label>Password</label>
+                  <Field type="password" name="password" />
+                  <ErrorMessage
                   name="password"
                   component="div"
                   className="form__error"
-                />
+                  />
 
-                <Button variant="primary" type="submit" disabled={isSubmitting}>
-                  SignUp
-                </Button>
-              </Form>
-            )}
-          </Formik>
+                  <Button variant="primary" type="submit" disabled={isSubmitting}>
+                    SignUp
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </Loading>
         </div>
       ) : (
         <div className="form__result--success">
@@ -119,6 +121,7 @@ const SignUp = () => {
         </div>
       )}
     </div>
+
   )
 }
 
