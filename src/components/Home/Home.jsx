1913b418dcd2a02794052cf563366423ca9993a6
felcg <xfelipecg@gmail.com>
2023-01-25 00:16:05 -0300

    remove recent games section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,14 +5,12 @@ import { Container } from 'react-bootstrap'
 import './Home.scss'
 import Hero from './Hero/Hero'
 import VideoContainer from './VideoContainer/VideoContainer'
-import RecentGames from './RecentGames/RecentGames'
 import TopRatedGames from './TopRatedGames/TopRatedGames'
 
 const Home = () => (
   <>
     <Hero />
     <Container className="contentWrapper">
-      <RecentGames />
       <TopRatedGames />
       <VideoContainer />
     </Container>
